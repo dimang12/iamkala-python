ad2d711d4a013087142316f94684bf6846ffcbb2
Dimang CHOU <dimang12@DimMacBookPro.local>
2017-12-22 12:58:43 -0800

    Added tech table into database

--- iamkala/models.py
+++ iamkala/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Tech(models.Model):
+    tech_title = models.CharField(max_length=255)
+    tech_highlight = models.TextField()
+    tech_full_desc = models.TextField()
+    tech_created_date = models.DateField()
+    tech_published_date = models.DateTimeField()
+    tech_ordering = models.IntegerField()
